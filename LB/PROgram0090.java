f0508f90de4cedd8841fe8ac4a9df6bc540c29c3
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-06-21 18:58:37 +0530

    initial commit

---
+++ LB/PROgram0090.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Logic
+{
+    public void calculatePower(int iBase, int iExpo) 
+    {
+        int iCnt = 0;
+        long iAns = 1;
+
+        for(iCnt = 1; iCnt <= iExpo; iCnt++)
+        {
+            iAns = iAns * iBase;
+        }
+
+        System.out.print("Answer is : "+iAns);
+
+    }
+}
+
+class PROgram0090
+{
+    public static void main(String Luffy[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        Logic lobj = new Logic();
+        
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.print("Enter the Value : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.print("Enter the Exponent : ");
+        iValue2 = sobj.nextInt();
+
+
+        lobj.calculatePower(iValue1, iValue2);
+
+
+    }
+}
