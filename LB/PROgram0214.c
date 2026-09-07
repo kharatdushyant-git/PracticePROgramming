a4eab2267725e8e7fb0ee2bc5d011839e51b76a0
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-07 19:45:44 +0530

    Add PROgram0214.c

---
+++ LB/PROgram0214.c
@@ -0,0 +1,40 @@
+/*
+
+Assignment 40
+
+Question 4:
+Write a recursive program which display below pattern.
+
+Input:
+6
+
+Output:
+A B C D E F
+
+*/
+
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    if(iNo == 0)
+    {
+        return;
+    }
+
+    Display(iNo - 1);
+    
+    printf("%c\t", 'A' + (iNo - 1));
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number: ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
