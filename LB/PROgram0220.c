08e0c8d706e3d1fe22ec17c97f6ffd1bb9c5bbc2
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-07 22:05:12 +0530

    Add PROgram0220.c

---
+++ LB/PROgram0220.c
@@ -0,0 +1,38 @@
+/*
+
+Assignment 41
+
+5. Write a recursive program which accept number from user
+   and return its product of digits.
+
+Input  : 523
+Output : 30
+
+*/
+
+#include <stdio.h>
+
+int Mult(int iNo)
+{
+    if(iNo == 0)
+    {
+        return 1;
+    }
+
+    return (iNo % 10) * Mult(iNo / 10);
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &iValue);
+
+    iRet = Mult(iValue);
+
+    printf("%d", iRet);
+
+    return 0;
+}
