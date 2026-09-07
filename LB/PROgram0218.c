3745811fe904c2735d753faaa62a8c8512b6e3e6
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-07 22:04:18 +0530

    Add PROgram0218.c

---
+++ LB/PROgram0218.c
@@ -0,0 +1,38 @@
+/*
+
+Assignment 41
+
+3. Write a recursive program which accept string from user
+   and count number of characters.
+
+Input  : Hello
+Output : 5
+
+*/
+
+#include <stdio.h>
+
+int Strlen(char *str)
+{
+    if(*str == '\0')
+    {
+        return 0;
+    }
+
+    return 1 + Strlen(str + 1);
+}
+
+int main()
+{
+    int iRet = 0;
+    char arr[20];
+
+    printf("Enter string: ");
+    scanf("%s", arr);
+
+    iRet = Strlen(arr);
+
+    printf("%d", iRet);
+
+    return 0;
+}
