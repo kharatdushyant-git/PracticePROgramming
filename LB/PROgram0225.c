9441e3da2bc50a404e506d3fd706ecd05a1ff6cd
Dushyant Kharat <kharatdushyant9@gmail.com>
2026-09-07 22:06:32 +0530

    Add PROgram0225.c

---
+++ LB/PROgram0225.c
@@ -0,0 +1,45 @@
+/*
+
+Asignment 42
+
+Question 5:
+Write a recursive program which accept number from user and return its reverse number.
+
+Input:
+523
+
+Output:
+325
+
+*/
+
+#include <stdio.h>
+
+int Reverse(int iNo)
+{
+    static int iRev = 0;
+
+    if(iNo == 0)
+    {
+        return iRev;
+    }
+
+    iRev = (iRev * 10) + (iNo % 10);
+
+    return Reverse(iNo / 10);
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number: ");
+    scanf("%d",&iValue);
+
+    iRet = Reverse(iValue);
+
+    printf("%d", iRet);
+
+    return 0;
+}
